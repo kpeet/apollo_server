937345b046497a4d573d306052e3d11a2d0e9100
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-23 18:04:34 -0400

    server listen was edited for heroku deploy test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 
 
 //arrancar el servidor
-server.listen().then( ({url}) => {
+server.listen({ port: process.env.PORT || 4000 }).then( ({url}) => {
     console.log(`Servidor listo en la url ${url}`)
 
 })
