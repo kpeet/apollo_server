040be19651be7db2b1ca76b9652392e4567348b4
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-25 23:14:40 -0400

    add jwt Bearer autentication

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
+const jwt = require('jsonwebtoken');
 
 const conectarDB = require('./config/db');
 
@@ -11,12 +12,31 @@ conectarDB();
 //servidor
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({req}) => {
+        console.log("Por Aqui");
+        const token = req.headers['autorization'] || '';
+        if(token){
+            try{
+                const usuario = jwt.verify(token.replace('Bearer ',''), process.env.SECRETA)
+                console.log(usuario);
+                return {
+                    usuario
+                }
+
+            }catch(error){
+                console.log('Hubo un error');
+                console.log(error)
+            }
+        }
+        console.log(token);
+
+    }
 });
 
 
 //arrancar el servidor
-server.listen({ port: process.env.PORT || 4000 }).then( ({url}) => {
+server.listen({port: process.env.PORT || 4000}).then(({url}) => {
     console.log(`Servidor listo en la url ${url}`)
 
 })
