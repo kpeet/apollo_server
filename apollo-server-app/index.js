0ca04445d45654b46fa0fea7f45fb5beed0d910d
ctroc <christopher.troc@gmail.com>
2020-06-05 21:23:37 -0400

    (add): finish middlewares auth implementation

--- apollo-server-app/index.js
+++ apollo-server-app/index.js
@@ -1,6 +1,10 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from 'apollo-server';
+import  {
+  RefreshTokenMiddleware,
+  setHeadersResponseMiddleware
+} from './src/middlewares/AuthTokenHandlerMiddleware';
 import dotenv from 'dotenv';
-dotenv.config(); /* Get env vars from .env file */
+dotenv.config();
 
 /* Here get yours class and functions from controllers */
 import  {
@@ -19,6 +23,13 @@ const server = new ApolloServer({
   dataSources: () => ({ /* Here subscribe yours dataSources */
    SrmAPI: new SrmAPI(),
   }),
+  context: ({ req , res }) => { /* middleware before request */
+    return RefreshTokenMiddleware(req, res);
+  },
+  formatResponse: (response, context) => { /* middleware after request */
+    setHeadersResponseMiddleware(context);
+    return response;
+  }
 });
 
 //Run Apollo Server
