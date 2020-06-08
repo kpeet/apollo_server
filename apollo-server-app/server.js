608f4b6c66faf048a3b5c9a97b3fdcdb023c82d7
Francisco Jordan <jupiter@Jupiter-Pro.local>
2020-06-08 16:42:30 -0400

    feat(main) update server.js from index.js

--- apollo-server-app/server.js
+++ apollo-server-app/server.js
@@ -1,23 +1,45 @@
 // server.js
 
-const { ApolloServer } = require('apollo-server-lambda');
-
+import { ApolloServer } from 'apollo-server-lambda';
 import  {
-  SrmAPI,
+  RefreshTokenMiddleware,
+  setHeadersResponseMiddleware
+} from './src/middlewares/AuthTokenHandlerMiddleware';
+import dotenv from 'dotenv';
+dotenv.config();
+
+/* Here get yours class and functions from controllers */
+import  {
+  SrmAUTH,
   AuthResolver,
   AuthSchema,
 } from './src/controllers/AuthController';
+import  {
+  SrmAPI,
+  TestResolver,
+  TestSchema,
+} from './src/controllers/TestController';
+
 /* Instance Apollo Server */
 const server = new ApolloServer({
   typeDefs: [  /* Here subscribe yours schemas */
     AuthSchema,
+    TestSchema,
   ],
   resolvers: [ /* Here subscribe yours resolvers mehtods */
     AuthResolver,
+    TestResolver,
   ],
   dataSources: () => ({ /* Here subscribe yours dataSources */
+   SrmAUTH: new SrmAUTH(),
    SrmAPI: new SrmAPI(),
   }),
+  context: ({ req , res }) => { /* middleware before request */
+    return RefreshTokenMiddleware(req);
+  },
+  formatResponse: (response, context) => { /* middleware after request */
+    return setHeadersResponseMiddleware(context, response);
+  }
 });
 
 exports.graphqlHandler = server.createHandler({
