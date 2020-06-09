8ca115e46738b19e057af3158dae6d3238066094
ctroc <christopher.troc@gmail.com>
2020-06-09 09:37:32 -0400

    (update): clear code

--- apollo-server-app/server.js
+++ apollo-server-app/server.js
@@ -5,8 +5,6 @@ import  {
   RefreshTokenMiddleware,
   setHeadersResponseMiddleware
 } from './src/middlewares/AuthTokenHandlerMiddleware';
-import dotenv from 'dotenv';
-dotenv.config();
 
 /* Here get yours class and functions from controllers */
 import  {
